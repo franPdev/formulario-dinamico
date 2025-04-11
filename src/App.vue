6b9ae28d3e91545ec6df524c8a8f05df24525339
franPdev <francoarielpaez@gmail.com>
2025-04-11 00:42:38 -0300

    proyecto base con Vue 3, TypeScript y Vuetify

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container class="py-4">
+
+        <v-btn color="primary">!Hola Vuetify!</v-btn>
+
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts"></script>
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import {createApp} from 'vue';
+import App from './App.vue';
+
+// Inportacion de vuetify 
+
+import {createVuetify} from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+// Creacion de la instancia de vuetify
+const vuetify = createVuetify({
+  components,   
+    directives,
+});
+
+const app = createApp(App)
+app.use(vuetify)
+app.mount('#app');

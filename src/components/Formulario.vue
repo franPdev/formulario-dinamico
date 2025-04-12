16fc26d55def255263454c187e871f7f1dcbdeb4
franPdev <francoarielpaez@gmail.com>
2025-04-12 18:54:45 -0300

    Se corrige la estructura del JSON y se completan validaciones dinámicas (required, minLength, regex, maxLength)

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -12,6 +12,8 @@
                 :type="campo.type"
                 :label="campo.label"
                 v-model="fromData[campo.name]"
+                :rules="getRules(campo)"
+                clearable
             
             ></v-text-field>
 
@@ -21,9 +23,11 @@
             v-else-if="campo.type === 'selectable'"
             :label="campo.label"
             :items="campo.options"
-            v-model="fromData[campo.name]">
-        
-            </v-select>
+            v-model="fromData[campo.name]"
+            :rules="getRules(campo)"
+                clearable
+            ></v-select>
+          
      
              </div>
 
@@ -42,6 +46,45 @@ import { reactive } from 'vue'
 // El objeto reactivo para almacenar los datos del formulario
 
 const fromData = reactive<Record<string, any>>({})
+
+
+//Funcion para getRules
+
+function getRules(campo: any){
+    const rules = []
+
+    //validaciones para los campos requeridos 
+
+    const required = campo.validations?.find((v: any) => v.type === 'required')
+    if (required) {
+        rules.push((v: string) => !!v || required.message)
+    }
+
+    //Validacion para minLegth
+
+    const minLength = campo.validations?.find((v: any) => v.type === 'minLength')
+    if (minLength) {
+        rules.push((v: string) => (v && v.length >= minLength.value) || minLength.message)
+    }
+
+    //Validacion para regex
+
+   const regex = campo.validations?.find((v: any) => v.type === 'regex')
+    if (regex) {
+     const pattern = new RegExp(regex.pattern)
+        rules.push((v: string) => (v && pattern.test(v)) || regex.message)
+    }
+
+
+    //Validacion para maxLength
+    const maxLength = campo.validations?.find((v: any) => v.type === 'maxLength')
+    if (maxLength) {
+     rules.push((v: any) => (!!v && String(v).length <= maxLength.value) || maxLength.message)
+
+    }
+
+    return rules
+}
 
 // Aca inicio los valores por defecto del JSON
 
